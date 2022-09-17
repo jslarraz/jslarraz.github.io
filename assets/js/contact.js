cb0a7137bdce5f4d0cc65fcfc7f7f0463de5836d
jslar <jslarraz@gmail.com>
2022-09-17 23:24:47 +0200

    code added to use emailjs for the contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -24,6 +24,47 @@ function contact_state(state) {
   }
 }
 
+
+// emailjs library specific
+(function() {
+  // Set up the API public key
+  emailjs.init('wCB2YJjjt5ftkjcQa');
+})();
+
+window.onload = function() {
+  this.sended = false;
+  var that = this;
+
+  var successMessage = "Message Send",
+      repeatMessage = "Message already Send",
+      warningMessage = "Something wrong! Please try later";
+
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    contact_state("loading");
+    if (!that.sended) {
+
+      // these IDs from the previous steps
+      emailjs.sendForm('service_s1o17pn', 'template_0ii5pbb', this)
+          .then(function () {
+            custom_alert(successMessage, "success");
+            contact_state("disable");
+          }, function (error) {
+            custom_alert(warningMessage, "error");
+            contact_state("disable");
+          });
+
+      that.sended = true;
+    } else {
+      custom_alert(repeatMessage, "success");
+      contact_state("disable");
+    }
+
+  });
+}
+
+/*
 $(function () {
   this.sended = false;
   var that = this;
@@ -33,7 +74,7 @@ $(function () {
     warningMessage = "Something wrong! Please try later";
 
   form.on("submit", function (event) {
-    /*Change URL address if you need*/
+    //Change URL address if you need
 
     contact_state("loading");
 
@@ -64,3 +105,6 @@ $(function () {
     event.preventDefault();
   });
 });
+*/
+
+
